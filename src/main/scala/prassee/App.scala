3450a7ea949f26922bc3d8d17aaf61d35e911e8e
prassee <prassee.sathian@gmail.com>
2013-09-28 22:09:11 +0530

    initial commit

--- src/main/scala/prassee/App.scala
+++ src/main/scala/prassee/App.scala
@@ -1,45 +1,13 @@
 package prassee
 
-import akka.actor.Actor
-import akka.actor.ActorSystem
-import akka.actor.Props
-import com.twitter.finagle.Service
-import org.jboss.netty.handler.codec.http.HttpRequest
-import org.jboss.netty.handler.codec.http.HttpResponse
-import org.jboss.netty.handler.codec.http.DefaultHttpResponse
-import org.jboss.netty.handler.codec.http.HttpVersion
-import org.jboss.netty.handler.codec.http.HttpResponseStatus
-import com.twitter.util.Future
-import java.net.InetSocketAddress
-import com.twitter.finagle.builder.ServerBuilder
-import com.twitter.finagle.http.Http
+import uk.co.bigbeeconsultants.http.HttpClient
+import java.net.URL
 
 object App {
   def main(args: Array[String]) {
-
-    val service = new Service[HttpRequest, HttpResponse] {
-      def apply(req: HttpRequest) = {
-        val r = req.getUri() match {
-          case "/" => {
-            println("init actor")
-            val actorRef = ActorSystem().actorOf(Props(new FirstActor()))
-            actorRef.!("ping")
-            new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-          }
-        }
-        Future.value(r)
-      }
-    }
-
-    val inet = new InetSocketAddress(10000)
-    ServerBuilder().codec(Http()).bindTo(inet).name("logingestorService").build(service)
+    val client = new HttpClient
+    val resp = client.get(new URL("http://www.prassee.com"))
+    println(resp.body.asString)
   }
 }
 
-class FirstActor extends Actor {
-  def receive = {
-    case "ping" => {
-      println("pong")
-    }
-  }
-}
