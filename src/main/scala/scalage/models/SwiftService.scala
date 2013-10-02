85310168d733dbb5fab35e00a13d9671eb1c2ec1
prassee <prassee.sathian@gmail.com>
2013-10-02 21:40:47 +0530

    renmaed older classes and gave some meanningful names

--- src/main/scala/scalage/models/SwiftService.scala
+++ src/main/scala/scalage/models/SwiftService.scala
@@ -7,49 +7,48 @@ import uk.co.bigbeeconsultants.http.header.HeaderName
 import uk.co.bigbeeconsultants.http.request.RequestBody
 import uk.co.bigbeeconsultants.http.header.MediaType
 
-object SwiftService extends App {
+object SwiftService {
+  private val client = new HttpClient
 
-  case class SwiftResponse(storageUrl: String, authToken: String, storageToken: String)
-
-  var resp = SwiftResponse("", "", "")
-  val client = new HttpClient
-  val swiftURL = "http://15.185.191.158:8080/auth/v1.0"
-
-  val respHeaders = (x: String) => client.get(
-    new URL(x), Headers(Map("x-auth-user" -> "admin:admin",
-      "x-auth-key" -> "admin"))).headers
-
-  respHeaders(swiftURL).foreach(f => {
-    f.name match {
-      case "X-Storage-Url" => resp = SwiftResponse(f.value, resp.authToken, resp.storageToken)
-      case "X-Auth-Token" => resp = SwiftResponse(resp.storageUrl, f.value, resp.storageToken)
-      case "X-Storage-Token" => resp = SwiftResponse(resp.storageUrl, resp.authToken, f.value)
-      case _ =>
-    }
-  })
-
-  println(resp)
-
-  // createContainer("scalage-swift")
-  listContainers
-  listObjectsInContainer("scalage")
-
-  def listContainers = {
-    val contList = client.get(new URL(resp.storageUrl + "?format=json"),
-      Headers(Map("X-Auth-Token" -> resp.authToken)))
-    println(contList.body.asString)
+  def getSwiftResponse = (x: SwiftAuthRequest) => {
+    var resp = SwiftAuthResponse("", "", "")
+    client.get(new URL(x.url),
+      Headers(Map("x-auth-user" -> x.name,
+        "x-auth-key" -> x.password))).headers.foreach(f => {
+        f.name match {
+          case "X-Storage-Url" =>
+            resp = SwiftAuthResponse(f.value, resp.authToken, resp.storageToken)
+          case "X-Auth-Token" =>
+            resp = SwiftAuthResponse(resp.storageUrl, f.value, resp.storageToken)
+          case "X-Storage-Token" =>
+            resp = SwiftAuthResponse(resp.storageUrl, resp.authToken, f.value)
+          case _ =>
+        }
+      })
+    resp
   }
+
+  def apply(swiftReq: SwiftAuthRequest,
+    httpClient: HttpClient) = new SwiftService(getSwiftResponse(swiftReq), client)
+
+}
+
+class SwiftService(swiftResponse: SwiftAuthResponse, client: HttpClient) {
+
+  def listContainers = client.get(new URL(swiftResponse.storageUrl + "?format=json"),
+    Headers(Map("X-Auth-Token" -> swiftResponse.authToken))).body.asString
 
   def createContainer(cont: String) = {
     val rb = RequestBody.apply("", MediaType.TEXT_PLAIN)
-    client.put(new URL(resp.storageUrl + "/" + cont), rb, Headers(Map("X-Auth-Token" -> resp.authToken)))
+    client.put(new URL(swiftResponse.storageUrl + "/" + cont), rb,
+      Headers(Map("X-Auth-Token" -> swiftResponse.authToken)))
   }
 
   def listObjectsInContainer(cont: String) = {
     val rb = RequestBody.apply("", MediaType.TEXT_PLAIN)
-    val containers = client.get(new URL(resp.storageUrl + "/" + cont), Headers(Map("X-Auth-Token" -> resp.authToken)))
-    println(containers.body)
+    val containers = client.get(new URL(swiftResponse.storageUrl + "/" + cont),
+      Headers(Map("X-Auth-Token" -> swiftResponse.authToken)))
+    containers.body
   }
-
 }
 
