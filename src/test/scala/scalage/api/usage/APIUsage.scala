def442ccf9b6f12fa534e9ac43e617934ea750bf
prassee <prassee.sathian@gmail.com>
2013-11-11 10:59:50 +0530

    changed group and artifact id and formatted some code

--- src/test/scala/scalage/api/usage/APIUsage.scala
+++ src/test/scala/scalage/api/usage/APIUsage.scala
@@ -10,8 +10,8 @@ import scalage.SwiftObject
 object APIUsage extends App {
 
   // connect to swift 
-  val account = connect(
-    auth("admin:admin", "admin", "http://15.185.191.158:8080/auth/v1.0"))
+  val account = connect(auth("admin:admin", "admin",
+    "http://15.185.191.158:8080/auth/v1.0"))
 
   // val cont2 =  account.createContainer("welcome3")
 
