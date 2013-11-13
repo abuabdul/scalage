435cb6a7042e06766b6296ab1a09785c48dada14
prassee <prassee.sathian@gmail.com>
2013-11-13 07:27:25 +0530

    added readme

--- src/test/scala/scalage/api/usage/APIUsage.scala
+++ src/test/scala/scalage/api/usage/APIUsage.scala
@@ -10,22 +10,23 @@ import scalage.SwiftObject
 object APIUsage extends App {
 
   // connect to swift 
+  val url = "http://15.185.162.30:8080/auth/v1.0"
+  // http://15.185.191.158:8080/auth/v1.0
   val account = connect(
-    auth("admin:admin", "admin", "http://15.185.191.158:8080/auth/v1.0"))
+    auth("admin:admin", "admin", url))
+  // val cont2 = account.createContainer("scala")
+  //  println(account.listContainers.foreach(obj => {
+  //    println(obj.name)
+  //  }))
 
-  // val cont2 =  account.createContainer("welcome3")
-
-  println(account.listContainers)
-
-  val cont2 = account.getContainer("scalage")
-  println(cont2.isDefined)
-
-  val container = cont2.get
-
-  container.listItems
-
-  val f2up = new File("ppt.pptx")
-  val swiftObj = SwiftObject("ppt", "ppt", f2up)
-  // container.uploadObject(swiftObj)
-  println(container.listItems)
+  val cont3 = account.getContainer("scalage")
+  println(cont3.isDefined)
+  //  //
+  //  val container = cont2.get
+  //  //  container.listItems
+  //  //
+  //  val f2up = new File("ppt.pptx")
+  //  val swiftObj = SwiftObject("ppt", "ppt", f2up)
+  //  container.uploadObject(swiftObj)
+  //  println(container.listItems)
 }
