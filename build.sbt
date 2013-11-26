cd9cced600826e8daf47e47b6f1e3a2419a575e7
prassee <prassee.sathian@gmail.com>
2013-11-26 20:43:40 +0530

    added build.sbt file and eclipse plugin file

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := "scala-swift"
+
+libraryDependencies ++= Seq(
+	"uk.co.bigbeeconsultants" % "bee-client_2.10" % "0.22.7",
+	"io.spray" % "spray-json_2.10" % "1.2.5",
+	"junit" % "junit" % "4.11" % "test",
+	"org.scalatest" % "scalatest_2.10" % "2.0.M6-SNAP8" % "test"
+)
+
+resolvers ++= Seq(
+	"big bee consultants" at "http://repo.bigbeeconsultants.co.uk/repo",
+	"spray" at "http://repo.spray.io"
+)
--- src/test/scala/scalage/api/usage/ScalageTest.scala
+++ src/test/scala/scalage/api/usage/ScalageTest.scala
@@ -2,48 +2,51 @@ package scalage.api.usage
 
 import java.io.File
 import org.junit.runner.RunWith
-import org.specs2.mutable.Specification
+import org.scalatest._
 import scalage.SwiftConnector
 import scalage.SwiftUploadObject
 import java.util.Date
-import org.specs2.runner.JUnitRunner
 import scalage.SwiftUploadObject
+import org.scalatest.junit.JUnitRunner
+import scalage.Account
 
 @RunWith(classOf[JUnitRunner])
-class ScalageSpecification extends Specification {
+class ScalageSpecification extends FlatSpec with Matchers {
+  var account: Option[Account] = None
+  var containerName = ""
+  var contcont = 0
+  "After login, Scalage's Account object" should "pass test " in {
+    val swiftCxn = SwiftConnector("admin:admin", "admin", "http://15.185.162.30:8080/auth/v1.0")
+    account = Option(swiftCxn.connect)
+    containerName = Fixture.containerName(contcont + 1)
+    contcont = account.get.listContainers.size
+  }
 
-  val swiftCxn = SwiftConnector("admin:admin", "admin", "http://15.185.162.30:8080/auth/v1.0")
-
-  val account = swiftCxn.connect
-
-  "After login, Scalage's Account object " should {
-
-    val contcont = account.listContainers.size
-    val containerName = Fixture.containerName(contcont + 1)
-
-    "list all containters" in {
-      assert(account.listContainers.isEmpty || !account.listContainers.isEmpty)
-    }
-
-    "create new Container" in {
-      val newCont = account.createContainer(containerName)
-      val cont = account.getContainer(containerName).get
-      assert(contcont + 1 == account.listContainers.size)
-    }
-
-    "The Container should upload object" in {
-      val cont = account.listContainers
-      // the code works if and only if we call the above listContainers method 
-      val curCont = account.getContainer(containerName).get
-      curCont.uploadObject(Fixture.swiftObj)
-    }
-
-    "The Container should list objects" in {
-      val cont = account.listContainers
-      val currCont = account.getContainer(containerName).get
-      currCont.listItems != null
+  it should "list all containters" in {
+    intercept[Exception] {
+      assert(account.get.listContainers.isEmpty || !account.get.listContainers.isEmpty)
     }
   }
+
+  it should "create new Container" in {
+    val newCont = account.get.createContainer(containerName)
+    val cont = account.get.getContainer(containerName).get
+    assert(contcont + 1 == account.get.listContainers.size)
+  }
+
+  it should "The Container should upload object" in {
+    val cont = account.get.listContainers
+    // the code works if and only if we call the above listContainers method 
+    val curCont = account.get.getContainer(containerName).get
+    curCont.uploadObject(Fixture.swiftObj)
+  }
+
+  it should "The Container should list objects" in {
+    val cont = account.get.listContainers
+    val currCont = account.get.getContainer(containerName).get
+    currCont.listItems != null
+  }
+
 }
 
 object Fixture {
